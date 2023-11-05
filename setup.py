ab952daf7b451a2ba184c6e8f1950d4fc5f4b2d1
Clement Ramirez <ramirez.clement3@gmail.com>
2023-11-05 17:08:17 +0100

    setup.py: add setuptools version requirement
    In some config, an earlier version of setuptools leads the building
    process to ingore the src directory.
    
    Signed-off-by: Clement Ramirez <ramirez.clement3@gmail.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude="tools"),
-    install_requires=['setuptools'],
+    install_requires=['setuptools>=68.2.2'],
     maintainer='7Robot',
     maintainer_email='7robot@bde.enseeiht.fr',
     description='TODO: Package description',
