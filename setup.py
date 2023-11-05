f477936c0f2f20fc1760ade9bba073bdc04d349c
Clement Ramirez <ramirez.clement3@gmail.com>
2023-11-05 13:23:02 +0100

    setup.*: add setup files for colcon builds
    Signed-off-by: Clement Ramirez <ramirez.clement3@gmail.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages, SetuptoolsDeprecationWarning
+import warnings
+
+warnings.filterwarnings("ignore", category=SetuptoolsDeprecationWarning)
+
+package_name = 'pigpiod_emulator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude="tools"),
+    install_requires=['setuptools'],
+    maintainer='7Robot',
+    maintainer_email='7robot@bde.enseeiht.fr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+)
